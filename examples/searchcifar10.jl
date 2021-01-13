8b0b6a303f250a789ca0ca0392d1849c2f6ea54c
Eric S. Tellez <donsadit@gmail.com>
2021-01-13 13:04:00 -0600

    more Pluto controls for cifar10

--- examples/searchcifar10.jl
+++ examples/searchcifar10.jl
@@ -34,11 +34,11 @@ This example shows how to create a NAPP over datasets of images easily.
 md"""
 As first step, you must download the dataset (CIFAR10) before fetching the train data, e.g.
 
-```MNIST.download()```
+```CIFAR10.download()```
 
 apparently, this doesn't work on Pluto and must be done using the terminal directly and accepting the terms and conditions of using the dataset.
 
-You can also accept it from Pluto passing keyworkd argument `i_accept_the_terms_of_use=true`, e.g., `MNIST.download(i_accept_the_terms_of_use=true)`.
+You can also accept it from Pluto passing keyworkd argument `i_accept_the_terms_of_use=true`, e.g., `CIFAR10.download(i_accept_the_terms_of_use=true)`.
 
 
 """
@@ -73,13 +73,25 @@ index = fit(DeloneInvIndex, l2_distance, X; numcenters=128, initial=:rand, maxit
 # ╔═╡ 5b743cbc-54fa-11eb-1be4-4b619e1070b2
 begin
 	sel = @bind example_symbol html"<input type='range' min='1' max='$n' step='1'>"
-	distsel = @bind distname PlutoUI.Select(["L2" => "Euclidean", "L1" => "Manhattan", "LInf" => "Chebyshev", "p0.5" => "Minkowski with p=0.5"])
+	distsel = @bind distname PlutoUI.Select([
+			"L2" => "Euclidean",
+			"L1" => "Manhattan",
+			"LInf" => "Chebyshev",
+			"p0.5" => "Minkowski with p=0.5"
+			])
 	
+	ksel = @bind k html"<input type='range' min='1' max='15' step='1' default='3'>"
 	
+	sep = Matrix{RGB}(undef, 32, 32) # separator
+	for i in eachindex(sep)
+		sep[i] = RGB(1.0, 1.0, 1.0)
+	end
+
 	md"""
-	## Query:
-	query id: $(sel),
+	
+	query id: $(sel) |
 	distance function: $(distsel)
+	k: $(ksel)
 	"""
 end
 
@@ -88,13 +100,9 @@ begin
 	dist = Dict("L2" => squared_l2_distance, "L1" => l1_distance, "LInf" => linf_distance, "p0.5" => lp_distance(0.5))[distname]
 	dist = 
 	# dist = lp_distance(2)
-	@time res = search(index, dist, X[example_symbol], KnnResult(10))
+	@time res = search(index, dist, X[example_symbol], KnnResult(k))
 	q = rgbimage(X[example_symbol]) # just to distinguish easily
-	s = Matrix{RGB}(undef, 32, 32)
-	for i in eachindex(s)
-		s[i] = RGB(1.0, 1.0, 1.0)
-	end
-	h = hcat(q, s, [rgbimage(X[p.id]) for p in res]...)
+	h = hcat(q, sep, [rgbimage(X[p.id]) for p in res]...)
 	
 	md"""
 
@@ -121,6 +129,6 @@ end
 # ╠═a23b0cae-455d-11eb-0e50-4dc31c050cc1
 # ╠═7d8db714-55c8-11eb-37a0-dd7e0744f6e1
 # ╠═1ce583f6-54fb-11eb-10ad-b5dc9328ca3b
-# ╟─5b743cbc-54fa-11eb-1be4-4b619e1070b2
-# ╟─def63abc-45e7-11eb-231d-11d94709acd3
+# ╠═5b743cbc-54fa-11eb-1be4-4b619e1070b2
+# ╠═def63abc-45e7-11eb-231d-11d94709acd3
 # ╠═c1b7e0ac-54fd-11eb-2153-b599f834da36

345a9b04d797d5cc5874a04165a5bf8746ff47b1
Eric S. Tellez <donsadit@gmail.com>
2022-04-06 10:19:55 -0500

    reduced test n

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,7 +40,7 @@ function runtest(; dim, n, m,
     recall = macrorecall(Igold, Ires)
     @info "AFTER optimization: $(index)" (recall=recall, qps=1/tsearchtime, gold_qps=1/gsearchtime)
     @info "searchtime: gold: $(gsearchtime * m), index: $(tsearchtime * m), optimization-time: $opttime"
-    
+
     @info "**** optimizing MinRecall(0.95) ****"
     opttime = @elapsed optimize!(index, MinRecall(0.95); verbose=false)
     @time Ires, Dres, tsearchtime = timedsearchbatch(index, Q, k; parallel=true)
@@ -55,7 +55,7 @@ end
     centersrecall = 0.95
     runtest(; dim=2, n=100, m=10, numcenters=10, k=3, centersrecall)
     @info "********************* Real search *********************"
-    runtest(; dim=8, n=10^6, m=1000, k=30, centersrecall, kbuild=3, ksearch=1)
+    runtest(; dim=8, n=10^5, m=1000, k=10, centersrecall, kbuild=3, ksearch=1)
     #optimize!(P, index, 0.9, verbose=true)
     #p = probe(P, index)
 
